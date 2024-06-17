daf8dbf384b6bebb24a83c9a318daf514e8419de
Davide-Morellato <morellato.davide@yahoo.it>
2024-06-17 20:44:00 +0200

    Change paginate from 5 to 4 into ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
     public function index(){
 
         // $projects = Project::all();
-        $projects = Project::with('technologies', 'type')->paginate(5);
+        $projects = Project::with('technologies', 'type')->paginate(4);
 
         return response()->json([
             'projects'=>$projects
